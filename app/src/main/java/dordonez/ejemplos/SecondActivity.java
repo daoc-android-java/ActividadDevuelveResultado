da24e9950f6a816e832e337b9e1a15939a6f3922
daoc <diego.ordonez@gmail.com>
2016-05-06 12:48:18 -0500

    Initial commit

---
+++ app/src/main/java/dordonez/ejemplos/SecondActivity.java
@@ -0,0 +1,42 @@
+package dordonez.ejemplos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        int num = intent.getIntExtra("numero", -1);
+        if(num < 0) {
+            Toast.makeText(this, "No llegó un entero", Toast.LENGTH_LONG).show();
+        } else {
+            TextView tv = (TextView) findViewById(R.id.tvNumero);
+            tv.setText(String.valueOf(num));
+        }
+    }
+
+    public void devolverCalculo(View view) {
+        TextView tv = (TextView) findViewById(R.id.tvNumero);
+        String str = tv.getText().toString();
+        try {
+            int num = Integer.parseInt(str);
+            num = (int) Math.pow(2, num);
+            Intent intent = new Intent();
+            intent.putExtra("resultado", num);
+            setResult(Activity.RESULT_OK, intent);
+            finish();
+        } catch(Exception e) {
+            Toast.makeText(this, "No llegó un entero", Toast.LENGTH_LONG).show();
+        }
+    }
+}
